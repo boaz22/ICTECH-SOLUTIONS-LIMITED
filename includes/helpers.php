e51ff99f7f25c3d3ee9595c8a5d49d42ca5e2cad
Abuta Kevin <abutakevin254@gmail.com>
2026-09-26 14:41:28 +0300

    commit

--- includes/helpers.php
+++ includes/helpers.php
@@ -160,20 +160,25 @@ function assetImageUrl($filename, $directory = 'images/') {
 }
 
 function partnerImageUrl($filename) {
-    $legacyNames = [
+    $builtInLogos = [
         'airtel.png' => 'partner-airtel-kenya.svg',
         'amazon-aws.png' => 'partner-amazon-aws.svg',
         'google-cloud.png' => 'partner-google-cloud.svg',
         'microsoft-azure.png' => 'partner-microsoft-azure.svg',
-        'safaricom.png' => 'partner-safaricom.png'
+        'safaricom.png' => 'partner-safaricom.png',
+        'partner-airtel-kenya.svg' => 'partner-airtel-kenya.svg',
+        'partner-amazon-aws.svg' => 'partner-amazon-aws.svg',
+        'partner-google-cloud.svg' => 'partner-google-cloud.svg',
+        'partner-microsoft-azure.svg' => 'partner-microsoft-azure.svg',
+        'partner-safaricom.png' => 'partner-safaricom.png'
     ];
 
     $filename = trim((string) $filename);
-    if (isset($legacyNames[$filename])) {
-        return assetImageUrl($filename);
+    if (isset($builtInLogos[$filename])) {
+        return assetImageUrl($builtInLogos[$filename]);
     }
 
-    return assetImageUrl($filename, 'images/partners/');
+    return SITE_URL . 'assets/images/partners/' . rawurlencode($filename);
 }
 
 function courseImageUrl($course) {
